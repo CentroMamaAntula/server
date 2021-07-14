a3fd12ef8a1e8ce4ccf0cc1e1f7e2fc1834a5247
CentroMamaAntula <85749037+CentroMamaAntula@users.noreply.github.com>
2021-07-14 16:30:42 -0300

    port 3000

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set("view engine", "ejs");
 //routes
 app.use('/api',require('./routes/router'));
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || "0.0.0.0";
 
 app.listen(server_port, server_host, () => {

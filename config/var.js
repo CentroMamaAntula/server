5c8b8653d394d3f60d0a7aea92957b96716f4f05
CentroMamaAntula <85749037+CentroMamaAntula@users.noreply.github.com>
2021-08-07 09:48:43 -0300

    db env tostring

--- config/var.js
+++ config/var.js
@@ -9,6 +9,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || '127.0.0.1',
   PORT: process.env.PORT || 4000,
-  DB_MONGO: process.env.DB_MONGO,
+  DB_MONGO: process.env.DB_MONGO.toString(),
   SECRETA : 'secretamamantula'
 }
